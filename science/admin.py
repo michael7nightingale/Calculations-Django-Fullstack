3a4d7831b0a0a625dac4d196695f591bbccc42e8
Michael Nightingale <suslanchikmopl@gmail.com>
2023-05-24 00:03:00 +0500

    Admin models for science

--- science/admin.py
+++ science/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+# from django.forms import model_to_dict
 
-# Register your models here.
+from .models import *
+
+
+def form_list_fields(model, exclude: set) -> list:
+    fields = set((field.name for field in model._meta.local_fields))
+    fields.difference_update(exclude)
+    return list(fields)
+
+
+@admin.register(Science)
+class ScienceAdmin(admin.ModelAdmin):
+    list_display = form_list_fields(model=Science, exclude={"content", })
+    list_display_links = ('title', 'slug')
+    ordering = ('id', )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = form_list_fields(model=Category, exclude={'content', })
+    list_display_links = ('title', 'slug')
+    ordering = ('id', )
+
+
+@admin.register(Formula)
+class FormulaAdmin(admin.ModelAdmin):
+    list_display = form_list_fields(model=Formula, exclude={'content', })
+    list_display_links = ('title', 'slug')
+    ordering = ('id', )
