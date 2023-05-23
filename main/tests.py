3ba7fe074fa2acceee206d4f686976d9288eb676
Michael Nightingale <suslanchikmopl@gmail.com>
2023-05-23 23:46:22 +0500

    Tests

--- main/tests.py
+++ main/tests.py
@@ -1,8 +1,7 @@
-import time
-
 from django.test import TestCase
 from aiohttp import ClientSession
 import asyncio
+import time
 
 
 async def test(url: str, n_request: int):
@@ -11,11 +10,11 @@ async def test(url: str, n_request: int):
         for _ in range(n_request):
             async with session.get(url) as response:
                 res = await response.text()
+        print(res)
 
     print(time.perf_counter() - time_started)
 
 
 if __name__ == '__main__':
-    url_ = "http://127.0.0.1:8000/science/physics/formulas/pressure_liquid/"
+    url_ = "http://127.0.0.1:8000/api/v1"
     asyncio.run(test(url_, 1000))
-
